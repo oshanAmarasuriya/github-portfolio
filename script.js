54d194057ef896debcb8b517a9eb3d3ff8ef8c15
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-21 19:47:20 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.getElementById('showPopupBtn').addEventListener('click', function() {
+  Swal.fire({
+    title: 'Pleasse be patient',
+    text: 'Sometimes it may take a while to start back-end of the system. Use Username : ramesh50 Password : 1234',
+    icon: 'info',
+    confirmButtonText: 'OK'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Redirect to the URL when the OK button is clicked
+      window.location.href = 'https://inventory-t4nh.onrender.com/'; 
+    }
+  });
+});
+
+
